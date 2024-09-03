cc1884ca58fff22f9e70aa4f80f96a04033268fb
ZAsprose <14989953+ZAsprose@users.noreply.github.com>
2024-09-03 09:56:36 +0800

    Add files via upload

---
+++ XunleiWebUI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="XunleiWebUI.MvcApplication" Language="C#" %>
---
+++ XunleiWebUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(XunleiWebUI.Startup))]
+namespace XunleiWebUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            //ConfigureAuth(app);
+        }
+    }
+}
